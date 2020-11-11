f04f3ad3773adba1dd767d891a4dffe8bde2663b
S4AGGY <ranierocj@gmail.com>
2020-11-11 22:46:57 +0300

    dropzone

--- src/main/java/com/example/templater/controller/FileUploadController.java
+++ src/main/java/com/example/templater/controller/FileUploadController.java
@@ -1,4 +1,20 @@
 package com.example.templater.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RestController
+@RequestMapping("/upload")
 public class FileUploadController {
+
+    @PostMapping
+    public void upload(@RequestParam("file") MultipartFile file) {
+        System.out.println("uploaded file " + file.getOriginalFilename());
+    }
 }
