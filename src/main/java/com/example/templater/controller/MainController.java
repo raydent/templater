764c6d6d3a59e2511670f92285563aed10761caf
fomichdenis <fomichdenis@gmail.com>
2021-01-19 17:09:36 +0300

    Angular and basic http auth changes

--- src/main/java/com/example/templater/controller/MainController.java
+++ src/main/java/com/example/templater/controller/MainController.java
@@ -1,30 +1,34 @@
 package com.example.templater.controller;
 
+import com.example.templater.model.TestClass;
 import com.example.templater.model.User;
 import com.example.templater.service.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.session.web.http.HeaderHttpSessionStrategy;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
+import java.util.Collections;
+import java.util.Map;
 import java.util.Objects;
 
 @Controller
 public class MainController {
     @Autowired
     private IUserService userService;
-    @Autowired
-    private AuthenticationManager authenticationManager;
+    //@Autowired
+    //private AuthenticationManager authenticationManager;
 
 //test
     @GetMapping("/login")
@@ -40,7 +44,7 @@ public class MainController {
         return "login";
     }
 
-    @PostMapping("/login")
+    /*@PostMapping("/login")
     public String setLogin(HttpServletRequest request, HttpServletResponse response, @ModelAttribute("userForm") User user,
                            BindingResult result) {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
@@ -50,7 +54,7 @@ public class MainController {
         SecurityContext sc = SecurityContextHolder.getContext();
         sc.setAuthentication(auth);
         return "redirect:/user";
-    }
+    }*/
 
 
     @GetMapping("/403")
@@ -69,8 +73,41 @@ public class MainController {
     }
 
     @GetMapping("/home")
-    public String home() {return "/home";}
+    public String home(Model model) {
+        model.addAttribute("userForm", new TestClass());
+        return "/home";
+    }
+
+    @GetMapping("/test")
+    public String test(Model model) {
+        model.addAttribute("userForm", new TestClass());
+        return "/test";
+    }
+
+    @PostMapping("/test")
+    public String postTest(@ModelAttribute("userForm") @Valid TestClass userForm, BindingResult bindingResult, Model model) {
+        userForm.setField1(userForm.getField() + "0");
+        return "/test";
+    }
+
 
     @GetMapping("/template")
     public String template() { return "/template";}
+
+    //@CrossOrigin(origins = "http://127.0.0.1:4200")
+    @GetMapping("/test_angular")
+    public @ResponseBody String test() {
+        System.out.println("my test");
+        return "{\"name\" : \"angular\"}";
+    }
+
+    @GetMapping("/token")
+    public Map<String,String> token(HttpSession session) {
+        return Collections.singletonMap("token", session.getId());
+    }
+
+    @Bean
+    HeaderHttpSessionStrategy sessionStrategy() {
+        return new HeaderHttpSessionStrategy();
+    }
 }
---
+++ src/main/java/com/example/templater/model/TestClass.java
@@ -0,0 +1,28 @@
+package com.example.templater.model;
+
+public class TestClass {
+    public String getField() {
+        return field;
+    }
+
+    public void setField(String field) {
+        this.field = field;
+    }
+
+    public String field;
+
+    public String getField1() {
+        return field1;
+    }
+
+    public void setField1(String field1) {
+        this.field1 = field1;
+    }
+
+    public String field1;
+
+    public TestClass() {
+        field = "5";
+        field1 = "50";
+    }
+}
